51de2116dd5627959ef8305cca5f968273d83876
Sharon Chang <changsharond@gmail.com>
2016-04-04 19:10:37 -0400

    testing stun server

--- public/simplewebrtc/script.js
+++ public/simplewebrtc/script.js
@@ -6,14 +6,23 @@ var webrtc = new SimpleWebRTC({
     // immediately ask for camera access
     autoRequestMedia: true,
 
+    // peerConnectionConfig: {
+    //   iceServers: [ {
+    //     url:'stun:stunserver.org'}, {
+    //     url: 'turn:192.158.29.39:3478?transport=tcp',
+    //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     username: '28224511:1379330808'}
+    //   ]
+    // }
+
     peerConnectionConfig: {
       iceServers: [ {
         url:'stun:stunserver.org'}, {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'}
-      ]
-    }
+          url: 'turn:192.158.29.39:3478?transport=tcp',
+          credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+          username: '28224511:1379330808'
+        }
+      ]}
 });
 
 // we have to wait until it's ready
