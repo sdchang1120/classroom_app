aefc4fa90c1aa1f0c7c2f53277fc8ca08fe4360f
Sharon Chang <changsharond@gmail.com>
2016-04-04 19:47:06 -0400

    stun/turn server config

--- public/simplewebrtc/script.js
+++ public/simplewebrtc/script.js
@@ -6,35 +6,16 @@ var webrtc = new SimpleWebRTC({
     // immediately ask for camera access
     autoRequestMedia: true,
 
-    peerConnectionConfig: {
-      iceServers: [ {
-        url:'stun:stunserver.org'}, {
-        url: 'turn:192.158.29.39:3478?transport=tcp',
-        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-        username: '28224511:1379330808'}
-      ]
-    }
-
     // peerConnectionConfig: {
     //   iceServers: [ {
     //     url:'stun:stunserver.org'}, {
-    //       url: 'turn:192.158.29.39:3478?transport=tcp',
-    //       credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-    //       username: '28224511:1379330808'
-    //     }
-    //   ]}
+    //     url: 'turn:192.158.29.39:3478?transport=tcp',
+    //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+    //     username: '28224511:1379330808'}
+    //   ]
+    // }
 
-    // var stun = {
-    //   'url': 'stun:myserver.com:3478'
-    // };
-    //
-    // var turn = {
-    //   'url': 'turn:myserver.com:3478',
-    //   'username': 'mysuser@myrealm',
-    //   'credential': 'mypassword'
-    // };
-    //
-    // peerConnectionConfig: { 'iceServers': [stun, turn] }
+    peerConnectionConfig: { iceServers: [ {url:'stun:stunserver.org'}, {url: 'turn:192.158.29.39:3478?transport=tcp', credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=', username: '28224511:1379330808'} ]}
 
 
 });
