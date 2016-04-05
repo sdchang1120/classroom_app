596d806e316b33ca4c5423ac860fa3ccf4157b40
Sharon Chang <changsharond@gmail.com>
2016-04-04 21:01:52 -0400

    adding other turn servers

--- public/simplewebrtc/script.js
+++ public/simplewebrtc/script.js
@@ -33,13 +33,13 @@ var webrtc = new SimpleWebRTC({
       //   credential: 'muazkh',
       //   username: 'webrtc@live.com'
       // }, {
-        url: 'turn:192.158.29.39:3478?transport=udp',
+        // url: 'turn:192.158.29.39:3478?transport=udp',
+        // credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        // username: '28224511:1379330808'
+      // }, {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
         credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
         username: '28224511:1379330808'
-      // }, {
-      //   url: 'turn:192.158.29.39:3478?transport=tcp',
-      //   credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-      //   username: '28224511:1379330808'
       } ]
     }
 
