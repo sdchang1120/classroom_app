0df00e925148cc8710a2476340950b94989893a9
Sharon Chang <changsharond@gmail.com>
2016-04-04 20:26:47 -0400

    adding other turn servers

--- public/simplewebrtc/script.js
+++ public/simplewebrtc/script.js
@@ -26,7 +26,21 @@ var webrtc = new SimpleWebRTC({
     // }
 
     peerConnectionConfig: {
-      iceServers: freeice()
+      iceServers: [ {
+        urls: "stun:23.21.150.121"
+      }, {
+        url: 'turn:numb.viagenie.ca',
+        credential: 'muazkh',
+        username: 'webrtc@live.com'
+      }, {
+        url: 'turn:192.158.29.39:3478?transport=udp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      }, {
+        url: 'turn:192.158.29.39:3478?transport=tcp',
+        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        username: '28224511:1379330808'
+      } ]
     }
 
 });
